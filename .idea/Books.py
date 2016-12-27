6e5023804bdb5c6155a62b9c6040920be39b4362
vihangi <vihangisandeep.vagal@my.jcu.edu.au>
2016-12-27 18:57:16 +0800

    Final coding is done
    Need to add algorithms and comments

--- .idea/Books.py
+++ .idea/Books.py
@@ -30,22 +30,28 @@ def read_file():
     global completed_books
     global index_value
     datum = []
+    all_books= []
     required = "r"
     completed = "c"
     file_pointer= open(FILENAME, "r")
     for index, data in enumerate(file_pointer.readlines()):
         data = data.strip()
         datum = data.split(",")
-        index_value = str(index)
-        datum.append(index_value)
-        print(datum)
+        #index_value = str(index)
+        #datum.append(index_value)
+        all_books.append(datum)
+    all_books.sort(key=itemgetter(1,2))
+    for i in range(len(all_books)):
+        index_value = str(i)
+        all_books[i].append(index_value)
+    for i in range(len(all_books)):
+        if all_books[i][3] == "r":
+            required_books.append(all_books[i])
+        if all_books[i][3] == "c":
+            completed_books.append(all_books[i])
 
-        if datum[3] == required:
-            required_books.append(datum)
-
-        if datum[3] == completed:
-            completed_books.append(datum)
     file_pointer.close()
+
 
 
 def display_menu():
@@ -53,16 +59,15 @@ def display_menu():
     The display_menu function is used to display the menu and checks whether the entered menu choice is valid
     :return: returns the menu choice of the user
     """
-    counter = True
-    print("")
-    print(""" Menu:
-                      R - List required books
-                      C - List completed books
-                      A - Add new book
-                      M - Mark a book as completed
-                      Q - Quit
 
-                      """)
+    counter = True
+    #print("")
+    print("""Menu:
+R - List required books
+C - List completed books
+A - Add new book
+M - Mark a book as completed
+Q - Quit """)
     menu_choice = input(">>>")
     menu_choice = menu_choice.lower()
     while(counter == True):
@@ -76,14 +81,12 @@ def display_menu():
 
         if (counter== True):
             print("Invalid menu choice")
-            print(""" Menu:
-                                  R - List required books
-                                  C - List completed books
-                                  A - Add new book
-                                  M - Mark a book as completed
-                                  Q - Quit
-
-                                  """)
+            print("""Menu:
+R - List required books
+C - List completed books
+A - Add new book
+M - Mark a book as completed
+Q - Quit """)
             menu_choice = input(">>>")
             menu_choice = menu_choice.lower()
 
@@ -93,60 +96,64 @@ def listing_books(list_book):
     total_pages=0
     count =0
     list_book.sort(key=itemgetter(4, 2))
+
     for i in range (len(list_book)):
-        print("{0}.{2:>10} {1:>10} {2:>10}by {3:20} {4:>20} pages".format(list_book[i][4],list_book[i][0],"",list_book[i][1], list_book[i][2]))
+        print(" {0}.  {1:40s} by {2:20s} {3:} pages".format(list_book[i][4],list_book[i][0],list_book[i][1], list_book[i][2]))
         total_pages = total_pages + int(list_book[i][2])
         count +=1
     if count==1 :
-        print("{:<10}Total pages for {} book : {}".format("", count, total_pages))
+        print("Total pages for {} book : {}".format(count, total_pages))
     elif count>1 :
-        print("{:<10}Total pages for {} books : {}".format("", count, total_pages))
+        print("Total pages for {} books : {}".format(count, total_pages))
     else:
         print("No books")
+
 
 def marking_books(required_books,completed_books):
 
     print("Enter the number of a book to mark as completed")
     count = 0
-    try:
-        marked_book_number = int(input(">>>"))
-        for i in range(len(required_books)):
-            if (marked_book_number == int(required_books[i][4])):
-                required_books[i][3] = "c"
-                completed_books.append(required_books[i])
-                required_books.pop(i)
-                print("{} by {} marked as completed".format(completed_books[i][0],completed_books[i][1]))
-                count+=1
-                break
-        if count == 0 :
-            for i in range(len(completed_books)):
-                if (marked_book_number == int(completed_books[i][4])):
-                    print("Book already completed")
+    while(True):
+        if len(required_books)==0:
+            break
+        try:
+            marked_book_number = int(input(">>>"))
+            for i in range(len(required_books)):
+                if (marked_book_number == int(required_books[i][4])):
+                    print("{} by {} marked as completed".format(required_books[i][0], required_books[i][1]))
+                    required_books[i][3] = "c"
+                    completed_books.append(required_books[i])
+                    required_books.pop(i)
                     count+=1
+                    break
+            if count>0 :
+                break
+            if count == 0 :
+                for i in range(len(completed_books)):
+                    if (marked_book_number == int(completed_books[i][4])):
+                        print("That book is already completed")
+                        count+=1
+                        break
+                break
 
-        if count == 0:
-            print("Invalid book number")
-            marking_books(required_books,completed_books)
+            if count == 0:
+                print("Invalid book number")
 
-
-    except ValueError:
-        if marked_book_number== 0:
-            pass
-        else:
+        except ValueError:
             print("Invalid input; enter a valid number")
-            marking_books(required_books,completed_books)
+
 
 def adding_books(index_value,required_books):
 
     data =[]
     title=input("Title:")
     while not title.strip():
-        print("Cannot be blank")
+        print("Input cannot be blank")
         title = input("Title:")
     data.append(title)
     author = input("Author:")
     while not author.strip():
-        print("Cannot be blank")
+        print("Input cannot be blank")
         author = input("Author:")
     data.append(author)
     while(True):
@@ -169,6 +176,7 @@ def adding_books(index_value,required_books):
     data.append("r")
     data.append(index_value)
     required_books.append(data)
+    print("{} by {} , ({} pages) added to reading list ".format(title,author,pages))
 
 def write_file():
     list_books=[]
@@ -178,7 +186,7 @@ def write_file():
     for i in range(len(completed_books)):
         list_books.append(completed_books[i])
     list_books.sort(key=itemgetter(4, 2))
-    print(list_books)
+
     outFile = open(FILENAME, "w")
 
     for i in range(len(list_books)):
@@ -201,7 +209,7 @@ def main():
 
     #printing the menu
     print("""Reading List 1.0 - by Vihangi Vagal
-    4 books loaded from {}""". format(FILENAME))
+{} books loaded from {}""". format(int(index_value)+1,FILENAME))
     menu_choice =display_menu()
     while(menu_choice != "q"):
         if menu_choice == "r":
@@ -213,7 +221,6 @@ def main():
             listing_books(completed_books)
             menu_choice = display_menu()
         elif menu_choice == "a":
-            print("Add books:")
             adding_books(index_value,required_books)
             menu_choice = display_menu()
         elif menu_choice == "m":
@@ -225,8 +232,7 @@ def main():
         else:
             print("invalid")
     if menu_choice == "q":
-        print("quit")
-        write_file()
+        print("{} books saved to {} \nHave a nice day :)".format(int(index_value)+1, FILENAME))
 
 main()
 
